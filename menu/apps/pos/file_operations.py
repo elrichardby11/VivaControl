9ee8873484d3c560b5983f1386532db7e5a741aa
RichardDev <richardmazuelos@gmail.cl>
2023-12-29 04:48:25 -0300

    separate functions, insert data and file operations

--- menu/apps/pos/file_operations.py
+++ menu/apps/pos/file_operations.py
@@ -1,45 +1,9 @@
 from datetime import datetime
-import cx_Oracle
-from dotenv import load_dotenv
-import os
+from menu.apps.pos.insert_data import insert_data
 
 def save_to_file(self,current_cart, total_amount, payment_method, payment_quantity, local, last_digit, total_amount_law, symbol):
 
-    load_dotenv() # Load database
-
-    # Conectar a la base de datos Oracle
-    con = cx_Oracle.connect(f"{os.getenv('NAME_DATABASE')}/{os.getenv('PASSWORD_DATABASE')}@XE")
-
-    # Crear un cursor
-    cursor = con.cursor()
-    
-    # Ejecutar la consulta SQL
-    cursor.execute("SELECT MAX(ID_MOVIMIENTO) FROM MOVIMIENTO")
-    
-    # Obtener los resultados de la consulta
-    result = cursor.fetchone()
-    max_id = result[0] if result[0] is not None else 0  # Maneja el caso en el que no haya registros
-    
-    # Incrementa el valor máximo en uno para obtener el nuevo ID
-    new_id = max_id + 1
-    fecha = datetime.now().strftime("%d/%m/%Y")
-    periodo = int(datetime.now().strftime("%Y"))
-    tipo_mov = 2    # Tipo 2 Venta
-    state = 1       # Estado Activo
-    rut = 1         # Rut generico Cliente
-    precio_total = total_amount if total_amount_law is None else total_amount_law
-
-    query = "INSERT INTO MOVIMIENTO (ID_MOVIMIENTO, FECHA, ID_TIPO_MOVIMIENTO, PERIODO, CSTATE_MOVIMIENTO, RUT_AUXILIAR, PRECIO_TOTAL) VALUES (:id, TO_DATE(:fecha, 'dd/mm/yyyy'), :tipo, :periodo, :state, :rut, :precio)"
-    cursor.execute(query, id=new_id, fecha=fecha, tipo=tipo_mov, periodo=periodo, state=state, rut=rut, precio=precio_total)
-    con.commit()
-
-    # Ciclo para detalle de movimiento, inserta 
-    for code, quantity in current_cart.items():
-        product_code = code
-        precio = self.products[code]["price"]
-        query = "INSERT INTO DETALLE_MOV (ID_MOV, ID_PROD, ID_SUCURSAL, CANTIDAD, PRECIO_UNITARIO) VALUES (:id_mov, :id_pro, :id_suc, :cant, :precio)"
-        cursor.execute(query, id_mov = new_id, id_pro = code, id_suc = local, cant = quantity, precio = precio)
-        con.commit()
+    insert_data(self, current_cart, total_amount, local, total_amount_law)
 
     now = datetime.now()
     # Nombre del Archivo
